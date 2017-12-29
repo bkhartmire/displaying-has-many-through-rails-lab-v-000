17d91be2c4e90edbf8d9afa9d4036fe25b13e0c8
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-29 00:40:52 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
 
   def datetime
-    self.appointment_datetime.strftime("%B %e, %Y at %:z")
+    self.appointment_datetime.strftime("%B %e, %Y at %H:%M")
   end
 end
